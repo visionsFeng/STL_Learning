67ed4c759514a6f13f6ac9ac5b867e4c573eb4df
Jim <fjm20000215@163.com>
2023-09-02 10:16:36 +0800

    First

---
+++ build/Test/CMakeFiles/stltest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stltest.dir/test.cpp.o"
+  "../../bin/stltest.pdb"
+  "../../bin/stltest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stltest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
