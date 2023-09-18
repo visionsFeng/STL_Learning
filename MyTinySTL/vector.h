69182b355ca6122e1f32f7d95540f71b661e0824
Jim <fjm20000215@163.com>
2023-09-18 20:26:44 +0800

    2023.9.18

--- MyTinySTL/vector.h
+++ MyTinySTL/vector.h
@@ -39,7 +39,191 @@ namespace mystl
 
 // 模板类: vector 
 // 模板参数 T 代表类型
+template <class T>
 
+class vector
+{
+    static_assert(!std::is_same<bool, T>::value, "vector<bool> is abandoned in mystl");
+public:
+    // vector 的嵌套型别定义
+    typedef mystl::allocator<T>                      allocator_type;
+    typedef mystl::allocator<T>                      data_allocator;
+
+    typedef typename allocator_type::value_type      value_type;
+    typedef typename allocator_type::pointer         pointer;
+    typedef typename allocator_type::const_pointer   const_pointer;
+    typedef typename allocator_type::reference       reference;
+    typedef typename allocator_type::const_reference const_reference;
+    typedef typename allocator_type::size_type       size_type;
+    typedef typename allocator_type::difference_type difference_type;
+
+    typedef value_type*                              iterator;
+    typedef const value_type*                        const_iterator;
+    typedef mystl::reverse_iterator<iterator>        reverse_iterator;
+    typedef mystl::reverse_iterator<const_iterator>  const_reverse_iterator;
+
+    allocator_type get_allocator() { return data_allocator(); }
+
+private:
+    iterator begin_;
+    iterator end_;
+    iterator cap_;
+
+public:
+    // 构造、复制、移动、析构函数
+    vector() noexcept
+    { try_init(); }
+
+    explicit vector(size_type n)
+    { fill_init(n, value_type()); }
+
+    vector(size_type n, const value_type& value) 
+    { fill_init(n, value); } 
+
+    template <class Iter, typename std::enable_if<mystl::is_input_iterator<Iter>::value, int>::type = 0>
+
+    vector(Iter first, Iter last) {
+        MYSTL_DEBUG(!(last < first));
+        range_init(first, last);
+    }
+
+    vector(const vecotr& rhs) {
+        range_init(rhs.begin_, rhs.end_);
+    }
+
+    vector(vector&& rhs) noexcept : begin_(rhs.begin_), end_(rhs.end_), cap_(rhs.cap_) {
+        rhs.begin_ = nullptr;
+        rhs.end_ = nullptr;
+        rhs.cap_ = nullptr;
+    }
+
+    vector(std::initializer_list<value_type> ilist) {
+        range_init(ilist.begin(), ilist.end());
+    }
+
+    vector& operator=(const vector& rhs);
+    vector& operator=(vector&& rhs) noexcept;
+
+    vector& operator=(std::initializer_list<value_type> ilist) {
+        vector tmp(ilist.begin(), ilist.end());
+        swap(tmp);
+        return *this;
+    }
+
+    ~vector() {
+        destroy_and_recover(begin_, end_, cap_ - begin_);
+        begin_ = end_ = cap_ = nullptr;
+    }
+
+public:
+
+  // 迭代器相关操作
+  iterator               begin()         noexcept 
+  { return begin_; }
+  const_iterator         begin()   const noexcept
+  { return begin_; }
+  iterator               end()           noexcept
+  { return end_; }
+  const_iterator         end()     const noexcept 
+  { return end_; }
+
+  reverse_iterator       rbegin()        noexcept 
+  { return reverse_iterator(end()); }
+  const_reverse_iterator rbegin()  const noexcept
+  { return const_reverse_iterator(end()); }
+  reverse_iterator       rend()          noexcept
+  { return reverse_iterator(begin()); }
+  const_reverse_iterator rend()    const noexcept 
+  { return const_reverse_iterator(begin()); }
+
+  const_iterator         cbegin()  const noexcept 
+  { return begin(); }
+  const_iterator         cend()    const noexcept
+  { return end(); }
+  const_reverse_iterator crbegin() const noexcept
+  { return rbegin(); }
+  const_reverse_iterator crend()   const noexcept
+  { return rend(); }
+
+ // 容量相关操作
+  bool      empty()    const noexcept
+  { return begin_ == end_; }
+  size_type size()     const noexcept
+  { return static_cast<size_type>(end_ - begin_); }
+  size_type max_size() const noexcept
+  { return static_cast<size_type>(-1) / sizeof(T); }
+  size_type capacity() const noexcept
+  { return static_cast<size_type>(cap_ - begin_); }
+  void      reserve(size_type n);
+  void      shrink_to_fit();
+
+  // 访问元素相关操作
+  reference operator[](size_type n)
+  {
+    MYSTL_DEBUG(n < size());
+    return *(begin_ + n);
+  }
+  const_reference operator[](size_type n) const
+  {
+    MYSTL_DEBUG(n < size());
+    return *(begin_ + n);
+  }
+  reference at(size_type n)
+  {
+    THROW_OUT_OF_RANGE_IF(!(n < size()), "vector<T>::at() subscript out of range");
+    return (*this)[n];
+  }
+  const_reference at(size_type n) const
+  {
+    THROW_OUT_OF_RANGE_IF(!(n < size()), "vector<T>::at() subscript out of range");
+    return (*this)[n];
+  }
+
+  reference front()
+  {
+    MYSTL_DEBUG(!empty());
+    return *begin_;
+  }
+  const_reference front() const
+  {
+    MYSTL_DEBUG(!empty());
+    return *begin_;
+  }
+  reference back()
+  {
+    MYSTL_DEBUG(!empty());
+    return *(end_ - 1);
+  }
+  const_reference back() const
+  {
+    MYSTL_DEBUG(!empty());
+    return *(end_ - 1);
+  }
+
+  pointer       data()       noexcept { return begin_; }
+  const_pointer data() const noexcept { return begin_; }
+
+  // 修改容器相关操作
+
+  // assign
+  void assign(size_type n, const value_type& value) {
+    fill_assign(n, value);
+  }
+
+  template <class Iter, typename std::enable_if<mystl::is_input_iterator<Iter>::value, int>::type = 0>
+  void assign(Iter first, Iter last) {
+    MYSTL_DEBUG(!(last < first));
+    copy_assign(first, last, iterator_category(first));
+  }
+
+  void assign(std::initializer_list<value_type> il) {
+    copy_assign(il.begin(), il.end(), mystl::forward_iterator_tag{});
+  }
+
+  // emplace / emplace_back
+
+
+}
 
 /*****************************************************************************************/
 
